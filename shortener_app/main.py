043b9771891bedcb5b56759c41d5b335f6352759
lunarmelon <lunarmelon@tuta.io>
2026-02-11 16:55:15 -0600

    feat(main): add short url forwarding

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,7 +1,8 @@
 import secrets
 
 import validators
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 
 from . import models, schemas
@@ -23,6 +24,11 @@ def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
 
 
+def raise_not_found(request):
+    message = f"URL '{request.url}' does not exist"
+    raise HTTPException(status_code=404, detail=message)
+
+
 @app.get("/")
 def read_root():
     return "Welcome to the URL shortener API"
@@ -40,7 +46,22 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     db.add(db_url)
     db.commit()
     db.refresh(db_url)
-    db.url.url = key
+    db_url.url = key
     db_url.admin_url = secret_key
 
     return db_url
+
+
+@app.get("/{url_key}")
+def forward_to_target_url(
+    url_key: str, request: Request, db: Session = Depends(get_db)
+):
+    db_url = (
+        db.query(models.URL)
+        .filter(models.URL.key == url_key, models.URL.is_active)
+        .first()
+    )
+    if db_url:
+        return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
