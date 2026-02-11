f95e280e1f78df8ec4077e290b5c667f0831c309
lunarmelon <lunarmelon@tuta.io>
2026-02-11 17:45:54 -0600

    feat(main): get url admin info

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -2,8 +2,10 @@ import validators
 from fastapi import Depends, FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
+from starlette.datastructures import URL
 
 from . import crud, models, schemas
+from .config import get_settings
 from .database import SessionLocal, engine
 
 app = FastAPI()
@@ -27,6 +29,16 @@ def raise_not_found(request):
     raise HTTPException(status_code=404, detail=message)
 
 
+def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
+    base_url = URL(get_settings().base_url)
+    admin_endpoint = app.url_path_for(
+        "administration info", secret_key=db_url.secret_key
+    )
+    db_url.url = str(base_url.replace(path=db_url.key))
+    db_url.admin_url = str(base_url.replace(path=admin_endpoint))
+    return db_url
+
+
 @app.get("/")
 def read_root():
     return "Welcome to the URL shortener API"
@@ -38,10 +50,7 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
         raise_bad_request(message="Your provided URL is not valid")
 
     db_url = crud.create_db_url(db=db, url=url)
-    db_url.url = key
-    db_url.admin_url = secret_key
-
-    return db_url
+    return get_admin_info(db_url)
 
 
 @app.get("/{url_key}")
@@ -50,5 +59,15 @@ def forward_to_target_url(
 ):
     if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
         return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
+
+
+@app.get(
+    "/admin/{secret_key}", name="administration info", response_model=schemas.URLInfo
+)
+def get_url_info(secret_key: str, request: Request, db: Session = Depends(get_db)):
+    if db_url := crud.get_db_url_by_secret_key(db, secret_key=secret_key):
+        return get_admin_info(db_url)
     else:
         raise_not_found(request)
